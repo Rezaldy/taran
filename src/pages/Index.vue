2506f0f38a82a16332b3d14c118237ba584ec170
Rizky Fakkel <rizky_fakkel@hotmail.com>
2020-02-28 17:00:50 +0100

    WIP

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,7 +23,15 @@
                 :icon="fasDice"
                 size="xl"
                 @click="calculateRng"
-              />
+              >
+                <q-tooltip
+                  anchor="top middle"
+                  transition-show="flip-right"
+                  transition-hide="flip-left"
+                >
+                  Roll the dice!
+                </q-tooltip>
+              </q-btn>
             </div>
           </div>
         </div>
@@ -85,37 +93,183 @@
         <div class="col col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
           <div class="widget-container">
             <div class="col-12 text-h2 text-center q-mb-md">Lootlist picker</div>
+            <q-dialog v-model="lootCallWinnerDialog">
+              <q-card>
+                <q-card-section class="row items-center q-pb-none">
+                  <div class="text-h6">THE WINNER IS...</div>
+                  <q-space />
+                  <q-btn icon="close" flat round dense v-close-popup />
+                </q-card-section>
+
+                <q-card-section>
+                  <h4 class="text-center">{{ lootCallWinner }}!!!</h4>
+                  <h6>Congratulations, I hope you're following.. And that you know where ToB is.</h6>
+                </q-card-section>
+              </q-card>
+            </q-dialog>
             <q-list bordered separator>
               <q-item
+                :class="{'bg-red-10': looter.active === false, 'bg-green-9': looter.active === true}"
                 clickable
                 v-ripple
-                v-for="(prize, index) in lootListOptions"
+                v-for="(looter, index) in lootListOptions"
                 :key="index"
-                :active="prize.active"
+                :active="looter.active"
+                @click="looter.active = !looter.active"
               >
                 <q-item-section avatar>
-                  <q-icon name="signal_wifi_off"/>
+                  <q-icon v-if="looter.active" name="fas fa-check"/>
+                  <q-icon v-else name="fas fa-times"/>
                 </q-item-section>
-                <q-item-section>Active</q-item-section>
-                <q-item-section side>Side</q-item-section>
+                <q-item-section>
+                  {{looter.name}}
+                </q-item-section>
+                <q-item-section>
+                  {{looter.call}}
+                </q-item-section>
               </q-item>
             </q-list>
+            <div class="row flex justify-evenly q-mt-md q-mb-md">
+              <q-btn
+                round
+                v-if="lootListOptions.length"
+                color="primary"
+                :icon="fasDice"
+                ref="lootInput"
+                size="sm"
+                @click="pickLootCaller"
+              >
+                <q-tooltip
+                  anchor="top middle"
+                  transition-show="flip-right"
+                  transition-hide="flip-left"
+                >
+                  Pick someone!
+                </q-tooltip>
+              </q-btn>
+
+              <q-btn
+                push
+                color="primary"
+                :icon="fasPlus"
+                v-if="lootListOptions.length"
+                @click="lootCallToAdd = {}"
+              >
+                <q-tooltip
+                  anchor="top middle"
+                  transition-show="flip-right"
+                  transition-hide="flip-left"
+                >
+                  Add lootcaller
+                </q-tooltip>
+                <q-popup-proxy transition-show="scale" transition-hide="scale">
+                  <q-input
+                    label="Name"
+                    v-model="lootCallToAdd.name"
+                    placeholder="Enter viewer name"
+                    ref="addLootCallName"
+                    class="q-ma-lg"
+                  />
+                  <q-input
+                    label="Loot call"
+                    v-model="lootCallToAdd.call"
+                    placeholder="Enter lootcall"
+                    ref="addLootCallCall"
+                    class="q-ma-lg"
+                  />
+                  <div class="row flex justify-center q-mt-sm q-mb-sm">
+                    <q-btn
+                      push
+                      color="primary"
+                      :icon="fasPlus"
+                      @click="addLootCall"
+                    />
+                  </div>
+                </q-popup-proxy>
+              </q-btn>
+              <q-btn
+                round
+                v-if="lootListOptions.length"
+                color="primary"
+                :icon="fasUndoAlt"
+                ref="lootInput"
+                size="sm"
+                @click="lootListOptions = []"
+              >
+                <q-tooltip
+                  anchor="top middle"
+                  transition-show="flip-right"
+                  transition-hide="flip-left"
+                >
+                  Reset
+                </q-tooltip>
+              </q-btn>
+            </div>
+            <div></div>
+            <div class="q-pa-md">
+              <q-input
+                v-if="!lootListOptions.length"
+                v-model="lootInput"
+                label="Loot list text"
+                bg-color="white"
+                placeholder="Paste loot list here"
+                text-color="white"
+                class="text-white"
+                filled
+                autogrow/>
+            </div>
+            <div class="row flex justify-evenly q-mt-md">
+              <q-btn
+                v-if="!lootListOptions.length"
+                round
+                color="primary"
+                :icon="fasSearch"
+                ref="lootInput"
+                size="xl"
+                @click="processLootInput"
+              >
+                <q-tooltip
+                  anchor="top middle"
+                  transition-show="flip-right"
+                  transition-hide="flip-left"
+                >
+                  Process list
+                </q-tooltip>
+              </q-btn>
+            </div>
 
             <div class="row flex justify-evenly q-mt-md">
               <q-btn
                 round
-                color="primary"
+                color="secondary"
                 :icon="fasList"
                 size="xl"
                 @click="openLootList"
-              />
+              >
+                <q-tooltip
+                  anchor="top middle"
+                  transition-show="flip-right"
+                  transition-hide="flip-left"
+                >
+                  Open loot list
+                </q-tooltip>
+              </q-btn>
               <q-btn
                 round
                 color="secondary"
                 :icon="fasUndoAlt"
+                disabled
                 size="xl"
                 @click="retrieveLootListOptions"
-              />
+              >
+                <q-tooltip
+                  anchor="top middle"
+                  transition-show="flip-right"
+                  transition-hide="flip-left"
+                >
+                  Refresh loot list
+                </q-tooltip>
+              </q-btn>
             </div>
           </div>
         </div>
@@ -165,8 +319,8 @@
 
 <script>
     import Vue from 'vue';
-    import { openURL } from 'quasar';
-    import {fasDice, fasUndoAlt, fasList} from '@quasar/extras/fontawesome-v5';
+    import {openURL} from 'quasar';
+    import {fasDice, fasUndoAlt, fasList, fasSearch, fasPlus} from '@quasar/extras/fontawesome-v5';
     import AnimatedNumber from "animated-number-vue";
     import VueLuckywheel from 'vue-luckywheel';
     import VueLuckywheelItem from 'vue-luckywheel';
@@ -265,6 +419,14 @@
                     },
                 ],
                 lootListOptions: [],
+                lootInput: '',
+                lootCallWinnerDialog: false,
+                lootCallWinner: '',
+                lootCallToAdd: {
+                    name: '',
+                    call: '',
+                    active: false,
+                },
             };
         },
         methods: {
@@ -301,24 +463,97 @@
                 openURL('https://twitch.center/customapi/quote/list?token=ba3ed13c');
             },
             retrieveLootListOptions() {
-                axios.get('//twitch.center/customapi/quote/list', {
-                    params: {
-                        token: 'ba3ed13c',
+                const testURL = '//twitch.center/customapi/quote/list?token=ba3ed13c';
+                const myInit = {
+                    method: 'GET',
+                    mode: 'no-cors',
+                };
+
+                const myRequest = new Request(testURL, myInit);
+
+                fetch(myRequest).then(function (response) {
+                    return response;
+                }).then(function (response) {
+                    console.log(response);
+                }).catch(function (e) {
+                    console.log(e);
+                });
+            },
+            processLootInput() {
+                // console.log(this.lootInput.split('\n'));
+                const lootCallers = [];
+                this.lootListOptions = [];
+
+                if (this.lootInput.length) {
+                    if (this.lootInput === 'There are no quotes added') {
+                        this.$q.notify({
+                            message: 'Did you read? The list is empty.',
+                            caption: 'Idiot.',
+                            type: 'warning'
+                        });
+                    } else {
+                        // \d\. a,(.*)\((.*)\) Regex for one line
+                        if (this.lootInput.match(/(?:\d\. a,(.*)\((.*)\)\|[\n]?)/gm)) {
+                            for (const lootCall of this.lootInput.match(/(?:\d\. a,(.*)\((.*)\)\|[\n]?)/gm)) {
+                                const lootCallMatch = lootCall.match(/\d\. a,(.*)\((.*)\)\|/);
+                                const lootCallObj = {
+                                    name: lootCallMatch[1],
+                                    call: decodeURI(lootCallMatch[2].charAt(0).toUpperCase() + lootCallMatch[2].slice(1)),
+                                    active: false,
+                                };
+                                this.lootListOptions.push(lootCallObj);
+                            }
+                            this.$q.notify({
+                                message: 'Loot processed!',
+                                caption: 'Select the people that chose correctly.',
+                                type: 'positive'
+                            });
+                        } else {
+                            this.$q.notify({
+                                message: 'Okay, cool, but I can\'t read that.',
+                                caption: 'Try again, but better.',
+                                type: 'negative'
+                            });
+                        }
                     }
-                }).then(
-                    (response) => {
-                        console.log(response.data);
-                    },
-                    (error) => {
-                        console.error(error);
-                    },
-                )
-            }
+                } else {
+                    this.$q.notify({
+                        message: 'WHERE IS THE DAMN LOOTLIST?',
+                        caption: 'Sit.',
+                        type: 'warning'
+                    });
+                }
+            },
+            pickLootCaller() {
+                const chosenCalls = this.lootListOptions.filter(function (o) {
+                    return o.active;
+                }).map(o => o.name);
+                this.lootCallWinner = chosenCalls[Math.floor(Math.random() * chosenCalls.length)];
+                this.lootCallWinnerDialog = true;
+            },
+            addLootCall() {
+                if (this.lootCallToAdd.name !== '' && this.lootCallToAdd.call) {
+                    this.lootListOptions.push({
+                        name:this.lootCallToAdd.name,
+                        call:this.lootCallToAdd.call,
+                        active:this.lootCallToAdd.active,
+                    });
+                    this.lootCallToAdd.name = '';
+                    this.lootCallToAdd.call = '';
+                } else {
+                    this.$q.notify({
+                        message: 'Check if you have all fields filled in.',
+                        type: 'negative'
+                    });
+                }
+            },
         },
         created() {
             this.fasDice = fasDice;
             this.fasUndoAlt = fasUndoAlt;
             this.fasList = fasList;
+            this.fasSearch = fasSearch;
+            this.fasPlus = fasPlus;
         }
     }
 </script>
